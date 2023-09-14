c360e36a006a100a79929ea0256ddcd0b4ba9843
Code1Pig <xjtu_cys001@163.com>
2023-09-14 13:32:40 +0800

    SoftMax Multiclass

--- Course-2-Advanced-Learning-Algorithms/Course-2-Week-2/Course-2-Week-2-Optional-Labs/lab_utils_multiclass_TF.py
+++ Course-2-Advanced-Learning-Algorithms/Course-2-Week-2/Course-2-Week-2-Optional-Labs/lab_utils_multiclass_TF.py
@@ -49,7 +49,7 @@ def plot_cat_decision_boundary_mc(ax, X, predict , class_labels=None, legend=Fal
     #ax.axis('tight')
 
       
-def plt_mc_data(ax, X, y, classes,  class_labels=None, map=plt.cm.Paired, 
+def plt_mc_data(ax, X, y, classes, class_labels=None, map=plt.cm.Paired, 
                 legend=False, size=50, m='o', equal_xy = False):
     """ Plot multiclass data. Note, if equal_xy is True, setting ylim on the plot may not work """
     for i in range(classes):
@@ -57,12 +57,12 @@ def plt_mc_data(ax, X, y, classes,  class_labels=None, map=plt.cm.Paired,
         col = len(idx[0])*[i]
         label = class_labels[i] if class_labels else "c{}".format(i)
         # this didn't work on coursera but did in local version
-        #ax.scatter(X[idx, 0], X[idx, 1],  marker=m,
-        #            c=col, vmin=0, vmax=map.N, cmap=map,
-        #            s=size, label=label)
         ax.scatter(X[idx, 0], X[idx, 1],  marker=m,
-                    color=map(col), vmin=0, vmax=map.N, 
-                    s=size, label=label)
+                   c=col, vmin=0, vmax=map.N, cmap=map,
+                   s=size, label=label)
+        # ax.scatter(X[idx, 0], X[idx, 1],  marker=m,
+        #             color=map(col), vmin=0, vmax=map.N, 
+        #             s=size, label=label)
     if legend: ax.legend()
     if equal_xy: ax.axis("equal")
 
